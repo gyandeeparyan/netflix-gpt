8c2551d984eb200d666c37dc3c415e3d536d17f0
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-08-30 15:35:53 +0530

    netflix-gpt : inception

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <>
+<h1 className="text-3xl text-center text-red-600 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
